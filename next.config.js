2b78cf3a789e96045fe3f61d5888185dcff12f49
Rohit Goyani <62933634+rohit-tatva@users.noreply.github.com>
2023-05-09 11:06:12 +0530

    first commit initial structuring

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    assetPrefix: undefined,
+    distDir: 'build'
+}
 
 module.exports = nextConfig
