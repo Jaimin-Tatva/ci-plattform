2ef35838734c07560aa99eb3528708022101c23d
Jaimin Vyas <jaymin.vyas@tatvasoft.com>
2023-05-23 14:48:01 +0530

    Customized design.

---
+++ pages/listingMission/index.tsx
@@ -0,0 +1,26 @@
+import CreateNewUserModal from "@/components/CreateNewUserModal";
+import ForgotPasswordModal from "@/components/ForgotPasswordModal";
+import ImageModal from "@/components/ImageModal";
+import SignInModal from "@/components/SignInModal";
+import { Grid } from "@mui/material";
+
+const createNewUser: React.FC = () => {
+
+    return (
+        <>
+            <div>
+                <Grid container spacing={1} minHeight={720}>
+                    <Grid item xs={8} style={{position: "relative"}}>
+                        <ImageModal imagePath="/login.png" fill={true} height={undefined} width={undefined} />
+                    </Grid>
+                    <Grid item xs={4} className="verticle-center">
+                        <CreateNewUserModal />
+                        <p>Privacy Policy</p>
+                    </Grid>
+                </Grid>
+            </div>
+        </>
+    )
+}
+
+export default createNewUser;
